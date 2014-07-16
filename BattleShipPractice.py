c613d7e26513f50915a51166983ba569b03290d6
AkiraMomoto <lm2817@nyu.edu>
2014-07-16 12:30:40 -0400

    Need to continue getting the rules

---
+++ BattleShipPractice.py
@@ -0,0 +1,12 @@
+# Levi Mollison
+# Currently learning how to program applications, starting with the easy Battleship game
+
+"""
+    Battleship Rules
+    
+    - Each opponent gets 5 ships
+    - The goal is to sink each players ships. Hitting a ship in all of its holes results in a sink
+    - Ships are layed in a  7 x 8 field
+    - Each player gets 5 ships
+"""
+
